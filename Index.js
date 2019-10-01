9bae667fa9d8f78a60f2dc601ff6365ac021114a
Pedro Augusto <pedro_valcarenghi@outlook.com>
2019-10-01 20:13:27 -0300

    codigo prolog rodando

--- Index.js
+++ Index.js
@@ -1,45 +1,41 @@
 // https://github.com/yagop/node-telegram-bot-api/blob/master/doc/usage.md#events
 
+//Require da api do BoT
 const TelegramBot = require('node-telegram-bot-api')
+//Uso do Token do Telegeram
 const TOKEN = '836807007:AAEA8rBgFFLCvOdpJ9bSz4VG8oNxE7xcR4Q' //BoT TOKEM NÂO MECHER POR FAVOR
 
+//Construção do BoT
 const bot = new TelegramBot(TOKEN, { polling: true })
 
-// bot.on('new_chat_members', (msg) => {
-//   bot.sendMessage(msg.chat.id, `Olá ${msg.from.first_name}, bem vindo ao Devs SC!! Conte-nos um pouco sobre você, com o que trabalha e onde, se possivel é claro`)
-// })
+//Console log das entradas das menssagens
+//bot.on( 'message', ( msg ) => console.log( 'msg', msg ) )
 
-bot.on( 'message', ( msg ) => console.log( 'msg', msg ) )
 
-// bot.on("message", ( msg ) => {
-//     let texto = bot.onText('texto', msg)
-// })
-
-// Teste Tau Prolog
-
+// Teste Tau-Prolog
 var pl = require('./node_modules/tau-prolog/modules/core.js');
-require('./node_modules/tau-prolog')
+require('./node_modules/tau-prolog/modules/lists.js')(pl)
 
 var session = pl.create(100);
 
 // Load the program
 var program =
-    "humano(joao)."
-    "humano(maria)."
-    "humano(pedro)."
-    "mulher(maria)."
-    "homem(joao)."
-    "homem(pedro)."
+    "humano(joao)." +
+    "humano(maria)." +
+    "humano(pedro)." +
+    "mulher(maria)." +
+    "homem(joao)." +
+    "homem(pedro).";
 session.consult(program);
-
-// Get Node.js argument: nodejs ./script.js item
-//var item = process.argv[2];
 
 // Query the goal
 session.query("humano(X).");
 
 // Show answers
- session.answers(x => console.log(pl.format_answer(x)));
+bot.on('message', (msg) => {
+    session.answers(x => bot.sendMessage(msg.chat.id, pl.format_answer(x)));
+})
 
-//bot.sendMessage(pl.format_answer(y)); //Text para mandar menssagems 
+// session.answers(x => console.log(pl.format_answer(x)));
+
 
