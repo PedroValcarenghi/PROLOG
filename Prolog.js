7e03fe953d792061f2ac302dbedc13efd59db21a
Aline Bazotti <alinebazotti@yahoo.com.br>
2019-10-05 08:47:02 -0300

    Correção músicas

--- Prolog.js
+++ Prolog.js
@@ -9,19 +9,14 @@ module.exports = {
         // Regras Prolog usar "+" entre as linhas e ";" no final
         // musica (Nome da Música, Gênero, Artista, Link do Spotify)
         var program =
-            "humano(joao)." +
-            "humano(maria)." +
-            "humano(pedro)." +
-            "mulher(maria)." +
-            "homem(joao)." +
-            "homem(pedro).";
+        "musica('Creep', ['Alternativo', 'Independente'], 'Radiohead', 'https://open.spotify.com/track/6b2oQwSGFkzsMtQruIWm2p?si=P65FyMvbTS6LqMTCGx9_dA').";
         session.consult(program);
 
         // Consulta 
-        session.query("humano(pedro).");
+        session.query("musica(X,Y,Z,A).");
         //Resposta
         session.answers(x => {
-                console.log('X == ',x)
+                console.log('X == ',pl.format_answer(x))
                 resp.push(x)
                
                 
