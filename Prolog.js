3c47d787753ad4df7b3602289c886f9d6b0d4c89
Pedro Augusto <pedro_valcarenghi@outlook.com>
2019-10-06 10:32:56 -0300

    Todas as musicas

--- Prolog.js
+++ Prolog.js
@@ -9,12 +9,19 @@ module.exports = {
         // Regras Prolog usar "+" entre as linhas e ";" no final
         // musica (Nome da Música, Gênero, Artista, Link do Spotify)
         var program =
+            //alternativo
+            "musica('Creep', ['Alternativo', 'Independente'], 'Radiohead', 'https://open.spotify.com/track/6b2oQwSGFkzsMtQruIWm2p?si=P65FyMvbTS6LqMTCGx9_dA')." +
+            "musica('Anna Júlia', ['Alternativo', 'Pop'], 'Los Hermanos', 'https://open.spotify.com/track/0aASUtDb1N96NJDwmWj5Gf?si=Cui489eGSnKIu04Bnq5HcA')." +
+            "musica('Losin My Religion', ['Alternativo', 'Rock'], 'REM', 'https://open.spotify.com/track/31AOj9sFz2gM0O3hMARRBx?si=YmnX_kNRTz2qP5hM349P9A')." +
+            "musica('Everybody Hurts', ['Alternativo', 'Rock'], 'REM', 'https://open.spotify.com/track/6PypGyiu0Y2lCDBN1XZEnP?si=nSRMKMEnT6G6kma_HV70hw')." +
+            "musica('Whats up?', ['Alternativo', 'Rock'], '4 Non Blondes', 'https://open.spotify.com/track/0jWgAnTrNZmOGmqgvHhZEm?si=m0UBwIoNRGWxIzCX3ODvyw')." +
+            "musica('Like a Stone', ['Alternativo', 'Rock'], 'Audioslave', 'https://open.spotify.com/track/3YuaBvuZqcwN3CEAyyoaei?si=kqbkvjFiQVSaqh_froIpvA')." +
+            "musica('Let Her Go',['Alternativo','Pop','Neofolk'],'Passenger','https://open.spotify.com/track/1RKtWeu7meOKfLS4OikFd8')." +
+            "musica('Take Me To Church',['Alternativo','Blues'],'Hozier','https://open.spotify.com/track/70LrxJ5u19umvrXbC19g20')." +
+            "musica('Seven Nation Army',['Alternativo','Rock'],'The White Stripes','https://open.spotify.com/track/7rbkDq2MsbWZAPiZv4uaQn')." +
+            "musica('Somewhere Only We Know',['Alternativo','Pop'],'Keane','https://open.spotify.com/track/0ll8uFnc0nANY35E0Lfxvg')." +
 
-            "musica('Creep', 'Alternativo', 'Radiohead', 'https://open.spotify.com/track/6b2oQwSGFkzsMtQruIWm2p?si=P65FyMvbTS6LqMTCGx9_dA')." +
-            "musica('Anna Júlia', 'Alternativo', 'Los Hermanos', 'https://open.spotify.com/track/0aASUtDb1N96NJDwmWj5Gf?si=Cui489eGSnKIu04Bnq5HcA')." +
-            "musica('Losin My Religion', 'Pop', 'REM', 'https://open.spotify.com/track/31AOj9sFz2gM0O3hMARRBx?si=YmnX_kNRTz2qP5hM349P9A')." +
-
-            "play(Z) :- musica(_,_,_,Z), Y=" + like + ".";
+        "play(Z) :- musica(_,_,_,Z), Y=" + like + ".";
 
         session.consult(program);
         // Consulta 
